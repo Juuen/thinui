b16972414f22c39b51085253eca1a200cfe0b22a
lijun <li.jun@demaotang.com>
2022-08-19 16:02:03 +0800

    test

--- bin/thin-cli.js
+++ bin/thin-cli.js
@@ -1,3 +1,9 @@
+const path = require("path");
+
 function main() {
+    let url = path.resolve("./");
     console.log("this is a thin ui.");
+    console.log("project path: ".concat(url));
 }
+
+main();
