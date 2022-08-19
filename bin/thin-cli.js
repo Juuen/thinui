5b22f75bb0a2dae9b2e53b7fc056f7d189d4930b
lijun <li.jun@demaotang.com>
2022-08-19 22:21:37 +0800

    功能优化

--- bin/thin-cli.js
+++ bin/thin-cli.js
@@ -1,9 +1,38 @@
+#!/usr/bin/env node
+
 const path = require("path");
 
-function main() {
+main({}, exit);
+
+/**
+ * Graceful exit for async STDIO
+ */
+
+function exit(code) {
+    // flush output for Node.js Windows pipe bug
+    // https://github.com/joyent/node/issues/6247 is just one bug example
+    // https://github.com/visionmedia/mocha/issues/333 has a good discussion
+    function done() {
+        if (!draining--) process.exit(code);
+    }
+
+    var draining = 0;
+    var streams = [process.stdout, process.stderr];
+
+    exit.exited = true;
+
+    streams.forEach(function (stream) {
+        // submit empty write request and wait for completion
+        draining += 1;
+        stream.write("", done);
+    });
+
+    done();
+}
+
+function main(options, done) {
     let url = path.resolve("./");
     console.log("this is a thin ui.");
     console.log("project path: ".concat(url));
+    done(0);
 }
-
-main();
