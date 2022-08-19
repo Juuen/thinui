16b3613813006e56162fbb5024d74229c9c8c051
lijun <li.jun@demaotang.com>
2022-08-20 00:59:05 +0800

    ui文件夹检查和创建

--- bin/thinui-cli.js
+++ bin/thinui-cli.js
@@ -4,13 +4,23 @@ const path = require("path");
 const { mkdir, access } = require("fs/promises");
 const { constants } = require("fs");
 
+main(process.argv.slice(2));
+
 async function main(argv) {
     let projectPath = path.resolve("./");
     if (!projectPath.endsWith("/")) projectPath = projectPath.concat("/");
 
-    let thinDir = await checkThinDir(projectPath);
-
-    console.log("this is a thin ui.");
+    let thinDir = await checkThinDir(projectPath),
+        thinuiDir = "";
+    console.log(thinDir);
+    thinuiDir = thinDir ? `${thinDir}/thinui` : `${projectPath}/thinbuilder/thinui`;
+    console.log(thinuiDir);
+    try {
+        await mkdir(thinuiDir, { recursive: true });
+    } catch (err) {
+        console.error(err);
+    }
+    console.log(thinDir);
     console.log("project path: ".concat(projectPath, ",argv:", argv));
 }
 
@@ -19,23 +29,14 @@ async function checkThinDir(base) {
 
     try {
         await access(base.concat("thinbuilder"), constants.R_OK | constants.W_OK);
-        thinDir = "thinbuilder";
+        thinDir = base.concat("thinbuilder");
     } catch {}
     if (thinDir) return thinDir;
 
     try {
         await access(base.concat("jsbuilder"), constants.R_OK | constants.W_OK);
-        thinDir = "jsbuilder";
+        thinDir = base.concat("jsbuilder");
     } catch {}
-    if (thinDir) return thinDir;
 
-    try {
-        await mkdir(base.concat("thinbuilder"), { recursive: true });
-        return "thinbuilder";
-    } catch (err) {
-        console.error(err);
-        return "";
-    }
+    return thinDir;
 }
-
-main(process.argv.slice(2));
